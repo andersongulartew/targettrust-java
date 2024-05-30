9ec08bf58536df99de904b3df3a7be9faf627212
andersongulartew <andersongularte1993@gmail.com>
2024-05-30 10:47:09 -0300

    .

--- Playground/src/br/com/targettust/calculadora/calcularIMC/Imc.java
+++ Playground/src/br/com/targettust/calculadora/calcularIMC/Imc.java
@@ -5,28 +5,43 @@ import java.util.Scanner;
 public class Imc {
 
     static Scanner sc = new Scanner(System.in);
-public static double Peso() {
 
-    System.out.print("Digite seu peso : ");
-    double peso = sc.nextDouble();
-    return peso;
-}
-public static double Altura() {
-    System.out.println("Digite sua altura");
-    double altura = sc.nextDouble();
-    return altura;
-}
+    public static double Peso() {
 
-public static void main(String[] args) {
+        System.out.print("Digite seu peso : ");
+        double peso = sc.nextDouble();
+        return peso;
+    }
+
+    public static double Altura() {
+        System.out.println("Digite sua altura");
+        double altura = sc.nextDouble();
+        return altura;
+    }
+
+    public static void main(String[] args) {
         double peso = Peso();
         double altura = Altura();
 
-        sc.close();
         double imc = peso / (altura * altura);
-        System.out.println("Seu IMC é: " + imc);
+        //String resultado = String.format("%.6f", imc);
+        System.out.println("Seu IMC é: " + imc) ;
 
+        if (imc <= 18.5) {
+            System.out.println("Sua Classificação é de Magreza");
+        } else if (imc <= 24.9) {
+            System.out.println("Sua Classificação Está Dentro do Normal");
+        } else if (imc <= 29.9) {
+            System.out.println("Sua Classificação é de Sobrepeso I");
+        } else if (imc <= 39.9) {
+            System.out.println("Sua Classificação é Obesidade II");
+        } else {
+            System.out.println("Sua Classificação é Obesidade Grave III");
+        }
 
-   }
-
+        sc.close();
+    }
 }
+
+
 
