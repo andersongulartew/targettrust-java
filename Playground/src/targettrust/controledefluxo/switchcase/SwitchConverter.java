96464569b7e664eafe79dbfc4611cc277b72e29b
andersongulartew <andersongularte1993@gmail.com>
2024-06-04 22:10:41 -0300

    atualização

---
+++ Playground/src/targettrust/controledefluxo/switchcase/SwitchConverter.java
@@ -0,0 +1,14 @@
+package targettrust.controledefluxo.switchcase;
+
+import java.util.Scanner;
+
+public class SwitchConverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // Escreva um programa que converta um valor em reais para uma das seguintes moedas: dólar, euro ou libra. O usuário deve escolher a moeda e informar o valor em reais
+
+        sc.close();
+
+    }
+}
---
+++ Playground/src/targettrust/loop/foreforEach/NumerosPares.java
@@ -0,0 +1,12 @@
+package targettrust.loop.foreforEach;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
