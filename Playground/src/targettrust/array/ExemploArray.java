324be97ae8bf916dbe7b0481cba0901496a4e743
andersongulartew <andersongularte1993@gmail.com>
2024-06-05 22:42:11 -0300

    atualização

--- Playground/src/targettrust/array/ExemploArray.java
+++ Playground/src/targettrust/array/ExemploArray.java
@@ -3,7 +3,7 @@ package targettrust.array;
 public class ExemploArray {
     public static void main(String[] args) {
         int[] num = new int[3];
-        num[0] = 0;
+        num[0] = 2;
         num[1] = 25;
         num[2] = 35;
 
