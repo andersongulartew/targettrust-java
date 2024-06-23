082effc9f0dd22fc0087197c87301dd48f524d5f
andersongulartew <andersongularte1993@gmail.com>
2024-06-23 19:46:58 -0300

    Adicionamos configurações do projeto e classes principais 🏗️✨
    Detalhes do Commit:  criamos e modificamos as classes Main, Endereco, Correntista e ContaBancaria para implementar a funcionalidade de contas bancárias.💚

--- POO/src/exercicios/ContaBancaria.java
+++ POO/src/exercicios/ContaBancaria.java
@@ -4,15 +4,32 @@ public class ContaBancaria {
     private double saldo;
     private  String titular;
 
-    public ContaBancaria(double saldo, String titular) {
+    private Correntista correntista;
+
+    public ContaBancaria(double saldo, String titular, Correntista correntista) {
         this.saldo = saldo;
         this.titular = titular;
+        this.correntista = correntista;
+    }
+
+     public double verificarSaldo(){
+        return saldo;
+    }
+    public String getTitular(){
+        return titular;
+    }
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public Correntista getCorrentista() {
+        return correntista;
     }
 
     public void depositar(double valor) {
         if(valor > 0){
             saldo += valor;
-            System.out.println("Depositado com sucesso!");
+            System.out.println("Valor depositado com sucesso!");
         }else{
             System.out.println("Valor insuficiente!");
         }
@@ -25,30 +42,6 @@ public class ContaBancaria {
             System.out.println("Valor do saque inválido");
         }
     }
-    public double verificarSaldo(){
-        return saldo;
-    }
-    public String getTitular(){
-        return titular;
-    }
-    public void setTitular(String titular){
-        this.titular = titular;
-    }
 
-    public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria(1500, "Joao");
-        conta.depositar(500);
-        System.out.println("saldo após deposito: " + conta.verificarSaldo());
-        System.out.println("----------------------------------------------");
-        conta.sacar(250);
-        System.out.println("Saldo pós sacar: " + conta.verificarSaldo());
-        System.out.println("----------------------------------------------");
-        conta.sacar(1800);
-        System.out.println("Saldo após tentativa de saque: " + conta.verificarSaldo());
-
-
-
-
-    }
 
 }
---
+++ POO/src/exercicios/Endereco.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+public class Endereco {
+
+    private String endereco;
+
+    public Endereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return endereco;
+    }
+
+
+}
